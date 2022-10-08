851c1ccb7176accc3c9c96fcb330bbac85d915ae
Able Arthy <ablearthy@gmail.com>
2022-10-09 00:14:03 +0300

    Implement expression evaluator

---
+++ src/Coxu/Eval.hs
@@ -0,0 +1,18 @@
+module Coxu.Eval (eval) where
+
+import Coxu.Parser
+import Coxu.Base.Ternary (to10)
+
+
+evalTerm :: Term -> Integer
+evalTerm (DecTerm t) = toInteger t
+evalTerm (TernTerm t) = to10 t
+evalTerm (ParensExpr e) = eval e
+
+eval :: Expr -> Integer
+eval (ExprTerm t) = evalTerm t
+eval (ExprNeg x) = negate (eval x)
+eval (ExprAdd x y) = eval x + eval y
+eval (ExprSub x y) = eval x - eval y
+eval (ExprMul x y) = eval x * eval y
+eval (ExprDiv x y) = eval x `div` eval y
